63d50f0d9c798582f7c66d0c8eafdb8a7c29c5ac
ggcraveiro <161388415+ggcraveiro@users.noreply.github.com>
2026-06-02 17:51:24 -0300

    Add menu2_page.dart (Map menu)

---
+++ lib/pages/menu2_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Menu2Page extends StatelessWidget {
+  const Menu2Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Map Menu'),
+    );
+  }
+}
