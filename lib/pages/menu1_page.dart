b2e5456e6f34cf5c64779c9562a6052afd092902
ggcraveiro <161388415+ggcraveiro@users.noreply.github.com>
2026-06-02 17:51:16 -0300

    Add menu1_page.dart (Home menu)

---
+++ lib/pages/menu1_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Menu1Page extends StatelessWidget {
+  const Menu1Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Menu'),
+    );
+  }
+}
