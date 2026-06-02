7d4f8b4ccbc950cde20d3b98ee8c1a5bd5b2c3a9
ggcraveiro <161388415+ggcraveiro@users.noreply.github.com>
2026-06-02 17:51:34 -0300

    Add menu4_page.dart (Incentives menu)

---
+++ lib/pages/menu4_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Menu4Page extends StatelessWidget {
+  const Menu4Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Incentives Menu'),
+    );
+  }
+}
