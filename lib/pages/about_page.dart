508ab410d492ace960393cb2e7bfab9f8db1ac02
ggcraveiro <161388415+ggcraveiro@users.noreply.github.com>
2026-06-02 22:30:31 -0300

    Change title text from 'About' to 'Sobre o app'

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -7,7 +7,7 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('About'),
+        title: const Text('Sobre o app'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
